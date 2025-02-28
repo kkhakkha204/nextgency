3641c2e74ce72dcb8b0ac78a6407db7289e98f57
Ad123 <kkhakkha204@gmail.com>
2025-02-28 21:14:01 +0700

    1

--- src/components/MusicToggle.jsx
+++ src/components/MusicToggle.jsx
@@ -8,8 +8,8 @@ const MusicToggle = () => {
 
     useEffect(() => {
         audioRef.current = new Audio("/assets/kiep-do-den-cover-cung-cac-hao-han-128-ytshorts.savetube.me.mp3");
-        audioRef.current.loop = true; // Lặp lại nhạc
-        audioRef.current.preload = "auto"; // Load nhạc trước để tránh delay
+        audioRef.current.loop = true;
+        audioRef.current.preload = "auto";
     }, []);
 
     const toggleMusic = () => {
@@ -26,8 +26,9 @@ const MusicToggle = () => {
 
     return (
         <motion.button
-            className="fixed top-5 right-5 w-14 h-14 rounded-full bg-[#f2c94d] border-4 border-[#394e2d] flex items-center justify-center shadow-lg text-[#394e2d]
-                 transition-all duration-300 hover:bg-[#394e2d] hover:text-[#f2c94d] z-30"
+            className="fixed top-5 right-5 w-14 h-14 rounded-full bg-[#f2c94d] border-4 border-[#394e2d] flex items-center justify-center
+                 shadow-lg text-[#394e2d] transition-all duration-300 hover:bg-[#394e2d] hover:text-[#f2c94d]
+                 z-50 pointer-events-auto"
             onClick={toggleMusic}
             whileHover={{ scale: 1.1, rotate: 10 }}
             whileTap={{ scale: 0.9 }}
